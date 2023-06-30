6d35f52e6ec1120b7da7a806cfdc2e8d11009e72
Benjy96 <benjamindickey@hotmail.co.uk>
2023-06-30 16:48:47 +0100

    Add 5s timeout to axios

--- document-library-frontend/src/utils/axiosInstance.js
+++ document-library-frontend/src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: (process.env.NODE_ENV === "development") ? 'https://localhost:7144/api/' : 'https://todo'
+    baseURL: (process.env.NODE_ENV === "development") ? 'https://localhost:7144/api/' : 'https://todo',
+    timeout: 5000
 });
 
 export default axiosInstance;
