62e177490d0e9d288fab4e9291ed4c7c2d8950ea
Benjy96 <benjamindickey@hotmail.co.uk>
2023-06-30 16:01:01 +0100

    Backend now stores files locally

--- DocumentLibrary.Backend/Controllers/DocumentsController.cs
+++ DocumentLibrary.Backend/Controllers/DocumentsController.cs
@@ -3,22 +3,47 @@ using Microsoft.AspNetCore.Mvc;
 namespace DocumentLibrary.Backend.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class DocumentsController : ControllerBase
 {
-    private readonly ILogger<DocumentsController> _logger;
+    private const string UploadDirectory = "upload";
 
-    public DocumentsController(ILogger<DocumentsController> logger)
+    public DocumentsController()
     {
-        _logger = logger;
     }
 
-    [HttpPost]
-    public IActionResult UploadDocument(IFormFile file)
+    [HttpPost("upload")]
+    public async Task<IActionResult> Upload(IFormFile file)
     {
-        // Handle the document upload logic here
-        // Save the file to the desired location
-        // Extract metadata like name, type, upload date, etc.
+        if (file == null || file.Length == 0)
+        {
+            return BadRequest("No file selected.");
+        }
+
+        if (!IsFileTypeValid(file))
+        {
+            return BadRequest("Invalid file type. Please select a PDF, Excel, Word, TXT, JPEG, or PNG file.");
+        }
+
+        var uploadPath = Path.Combine(Directory.GetCurrentDirectory(), UploadDirectory);
+        Directory.CreateDirectory(uploadPath); // Create the directory if it doesn't exist
+
+        var filePath = Path.Combine(uploadPath, file.FileName);
+
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+
         return Ok();
+    }
+
+    private bool IsFileTypeValid(IFormFile file)
+    {
+        var allowedFileTypes = new[] { ".pdf", ".xls", ".xlsx", ".doc", ".docx", ".txt", ".jpg", ".jpeg", ".png" };
+
+        var fileExtension = Path.GetExtension(file.FileName);
+
+        return allowedFileTypes.Contains(fileExtension.ToLower());
     }
 }
